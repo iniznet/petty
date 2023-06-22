21b0aeb3317171add4daaacd8000aff6fd4ca946
niznet <iniznet@gmail.com>
2023-06-23 02:56:10 +0700

    fix(migrations): missing some cols for foreign key

--- database/migrations/4_0_0_create_chapters_table.php
+++ database/migrations/4_0_0_create_chapters_table.php
@@ -12,17 +12,19 @@ return new class extends Migration
 	{
 		Schema::create('chapters', function (Blueprint $table) {
 			$table->id();
-			$table->foreign('series_id')->references('id')->on('series')->onDelete('cascade');
-			$table->foreign('group_id')->references('id')->on('group')->onDelete('cascade');
 			$table->text('title');
 			$table->string('slug', 50);
 			$table->integer('volume', 3)->nullable();
 			$table->integer('chapter', 3)->nullable();
 			$table->integer('part', 3)->nullable();
 			$table->text('url');
+			$table->unsignedBigInteger('series_id');
+			$table->unsignedBigInteger('group_id');
 			$table->unsignedBigInteger('author_id');
 			$table->timestamps();
 
+			$table->foreign('series_id')->references('id')->on('series')->onDelete('cascade');
+			$table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
 			$table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
 		});
 	}
