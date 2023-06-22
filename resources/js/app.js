6276cbe97abd0f1a326a1881e185ba1bf64aa2df
niznet <iniznet@gmail.com>
2023-06-23 02:58:56 +0700

    feat(javascript): flamethrower router

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import "vite/modulepreload-polyfill";
 import "@/scss/app.scss";
 
 import alpine from "alpinejs";
+import flamethrower from "flamethrower-router";
 
 window.Alpine = alpine;
 alpine.start();
+flamethrower({
+    prefetch: true,
+});
